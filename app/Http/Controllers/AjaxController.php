bd876187e1107506034a0a8fb7c6547d6af14ac6
Rahul4045 <rahul015919@gmail.com>
2023-08-21 00:43:53 +0530

    first commit

---
+++ app/Http/Controllers/AjaxController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AjaxController extends Controller
+{
+    public function index()
+    {
+        return view('ajax.index');
+    }
+    public function users()
+    {
+        return view('users.userpage');
+    }
+}
